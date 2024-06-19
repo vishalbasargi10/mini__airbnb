e67511e18af4e2205a59a3968b0d4e72372196d7
vishalbasargi10 <vishalrajkumarbasargi@gmail.com>
2024-06-19 15:08:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const passport=require("passport")
+const LocalStratergy=require("passport-local")
+const User=require("./models/user.js");
+const  session  = require("express-session");
+
+
+const userRouter=require("./routes/user.js")
+
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+
+
+
+app.use(session(sessionOptions));  
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratergy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.get("/demouser",async(req,res)=>{
+  let fakeUser=new User({
+    email:"vis@gmail.com",
+    username:"delta"
+  });
+  let registerUser=await User.register(fakeUser,"hello");
+  res.send(registerUser);
+});
+
+app.use("/",userRouter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// //Create Route
+// app.post("/listings", async (req, res) => {
+//   const newListing = new Listing(req.body.listing);
+//   await newListing.save();
+//   res.redirect("/listings");
+// });
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/signup',(req,res)=>{
+    res.send("form")
+})
+module.exports=router;
